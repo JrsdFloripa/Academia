e0b142019becf24b37525c6ad6b454fec1bb314e
Leonardo-ltgm <111448089+Leonardo-ltgm@users.noreply.github.com>
2023-09-03 08:43:05 -0300

    Leonardo-ltgm

--- Academia/src/main/java/com/example/academia/Controller/GerControler.java
+++ Academia/src/main/java/com/example/academia/Controller/GerControler.java
@@ -1,14 +1,34 @@
 package com.example.academia.Controller;
 
+import com.example.academia.Entity.Administracao;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.GerenciamentoService;
 
 @RestController
 @RequestMapping("/api/academia")
 public class GerControler {
+
+    private final GerenciamentoService gerenciamentoService;
+
+    public GerControler(GerenciamentoService gerenciamentoService) {
+        this.gerenciamentoService = gerenciamentoService;
+    }
+
+
     @GetMapping("/malhacao")
-    public String Malhacao(){
+    public ResponseEntity<String> Malhacao(){
         return "malhando cabecao";
     }
+    try {
+        gerenciamentoService.salvarAdministracao(administracao);
+    }catch (Exception exception)
+        return new ResponseEntity<>("Erro",HttpStatus.BAD_REQUEST);
+    }
+    return new ResponseEntity<>("Administracao cadastrada com sucesso", HttpStatus.CREATED);
 }
+
--- Academia/src/main/java/com/example/academia/Entity/Aula.java
+++ Academia/src/main/java/com/example/academia/Entity/Aula.java
@@ -5,13 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Aula {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String aula;
     private String turno;
     private Administracao matricula;
+
+    @OneToMany(mappedBy = "aula")
+    private List<Aluno> alunos;
 }
